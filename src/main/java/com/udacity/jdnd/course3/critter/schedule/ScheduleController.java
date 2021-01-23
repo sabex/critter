720d9cce509f3d858d99e5f9f9539d7cfdac73e6
Raymond Bleach <rableach@yahoo.com>
2021-01-23 13:41:31 -0700

    adds postman, refactors controller

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,43 +1,110 @@
+/* (C)2021 */
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.service.EmployeeService;
+import com.udacity.jdnd.course3.critter.service.PetService;
 import com.udacity.jdnd.course3.critter.service.ScheduleService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-/**
- * Handles web requests related to Schedules.
- */
+/** Handles web requests related to Schedules. */
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
 
-    @Autowired
-    private ScheduleService scheduleService;
+  @Autowired private ScheduleService scheduleService;
 
-    @PostMapping
-    public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        return scheduleService.createSchedule(scheduleDTO);
-    }
+  @Autowired private ModelMapper modelMapper;
 
-    @GetMapping
-    public List<ScheduleDTO> getAllSchedules() {
-        return scheduleService.getAllSchedules();
-    }
+  @Autowired PetService petService;
 
-    @GetMapping("/pet/{petId}")
-    public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
-        return scheduleService.getAllSchedulesForPet(petId);
-    }
+  @Autowired EmployeeService employeeService;
 
-    @GetMapping("/employee/{employeeId}")
-    public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
-        return scheduleService.getAllSchedulesForEmployee(employeeId);
-    }
+  @PostMapping
+  public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
+    return toDto(scheduleService.createSchedule(toEntity(scheduleDTO)));
+  }
 
-    @GetMapping("/customer/{customerId}")
-    public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
-        return scheduleService.getAllSchedulesForCustomer(customerId);
+  @GetMapping
+  public List<ScheduleDTO> getAllSchedules() {
+    List<Schedule> scheduleList = scheduleService.getAllSchedules();
+    return scheduleList.stream().map(this::toDto).collect(Collectors.toList());
+  }
+
+  @GetMapping("/employee/{employeeId}")
+  public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
+    Employee employee = employeeService.get(employeeId);
+    List<Schedule> scheduleList = scheduleService.getAllSchedulesForEmployee(employee);
+    return scheduleList.stream().map(this::toDto).collect(Collectors.toList());
+  }
+
+  @GetMapping("/pet/{petId}")
+  public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
+    Pet pet = petService.get(petId);
+    List<Schedule> scheduleList = scheduleService.getAllSchedulesForPet(pet);
+    return scheduleList.stream().map(this::toDto).collect(Collectors.toList());
+  }
+
+  @GetMapping("/customer/{customerId}")
+  public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
+    List<Pet> pets = petService.findAllByOwnerId(customerId);
+    // for each pet, get all schedules
+    List<Schedule> scheduleList = new ArrayList<>();
+    for (Pet pet : pets) {
+      scheduleList.addAll(scheduleService.getAllSchedulesForPet(pet));
     }
+    return scheduleList.stream().distinct().map(this::toDto).collect(Collectors.toList());
+  }
+
+  /**
+   * converts Entity to Dto
+   *
+   * @param schedule
+   * @return
+   */
+  private ScheduleDTO toDto(Schedule schedule) {
+    ScheduleDTO dto = modelMapper.map(schedule, ScheduleDTO.class);
+    dto.setEmployeeIds(
+        schedule.getScheduledEmployees().stream()
+            .map(employee -> employee.getId())
+            .collect(Collectors.toList()));
+    dto.setPetIds(
+        schedule.getScheduledPets().stream().map(pet -> pet.getId()).collect(Collectors.toList()));
+    return dto;
+  }
+
+  /**
+   * converts dto into Entity
+   *
+   * @param dto
+   * @return
+   */
+  private Schedule toEntity(ScheduleDTO dto) {
+    Schedule schedule = modelMapper.map(dto, Schedule.class);
+    // add other entities
+    List<Employee> employeeList =
+        dto.getEmployeeIds().stream()
+            .map(
+                empId -> {
+                  return employeeService.get(empId);
+                })
+            .collect(Collectors.toList());
+    schedule.setScheduledEmployees(employeeList);
+    List<Pet> petList =
+        dto.getPetIds().stream()
+            .map(
+                petId -> {
+                  return petService.get(petId);
+                })
+            .collect(Collectors.toList());
+    schedule.setScheduledPets(petList);
+    return schedule;
+  }
 }
--- src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -1,117 +1,53 @@
+/* (C)2021 */
 package com.udacity.jdnd.course3.critter.service;
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.entity.Schedule;
 import com.udacity.jdnd.course3.critter.repository.ScheduleRepository;
-import com.udacity.jdnd.course3.critter.schedule.ScheduleDTO;
+import java.util.List;
+import javax.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ScheduleService {
 
-    private EmployeeService employeeService;
-    private PetService petService;
+  private EmployeeService employeeService;
+  private PetService petService;
 
-    private ScheduleRepository scheduleRepository;
-    private ModelMapper modelMapper;
+  private ScheduleRepository scheduleRepository;
+  private ModelMapper modelMapper;
 
-    public ScheduleService(EmployeeService employeeService, PetService petService, ScheduleRepository scheduleRepository, ModelMapper modelMapper) {
-        this.employeeService = employeeService;
-        this.petService = petService;
-        this.scheduleRepository = scheduleRepository;
-        this.modelMapper = modelMapper;
-    }
+  public ScheduleService(
+      EmployeeService employeeService,
+      PetService petService,
+      ScheduleRepository scheduleRepository,
+      ModelMapper modelMapper) {
+    this.employeeService = employeeService;
+    this.petService = petService;
+    this.scheduleRepository = scheduleRepository;
+    this.modelMapper = modelMapper;
+  }
 
-    public List<ScheduleDTO> getAllSchedules() {
-        List<Schedule> scheduleList = scheduleRepository.findAll();
-        return scheduleList
-                .stream()
-                .map(this::toDTo)
-                .collect(Collectors.toList());
-    }
+  public List<Schedule> getAllSchedules() {
+    return scheduleRepository.findAll();
+  }
 
-    public List<ScheduleDTO> getAllSchedulesForEmployee(Long employeeId) {
-        Employee employee = employeeService.get(employeeId);
-        List<Schedule> scheduleList = scheduleRepository.findByScheduledEmployees(employee);
-        return scheduleList
-                .stream()
-                .map(this::toDTo)
-                .collect(Collectors.toList());
-    }
+  public List<Schedule> getAllSchedulesForEmployee(Employee employee) {
+    return scheduleRepository.findByScheduledEmployees(employee);
+  }
 
-    public List<ScheduleDTO> getAllSchedulesForPet(Long petId) {
-        Pet pet = petService.get(petId);
-        List<Schedule> scheduleList = scheduleRepository.findByScheduledPets(pet);
-        return scheduleList
-                .stream()
-                .map(this::toDTo)
-                .collect(Collectors.toList());
-    }
+  public List<Schedule> getAllSchedulesForPet(Pet pet) {
+    return scheduleRepository.findByScheduledPets(pet);
+  }
 
-    public List<ScheduleDTO> getAllSchedulesForCustomer(Long customerId) {
-        List<Pet> pets = petService.findAllByOwnerId(customerId);
-        // for each pet, get all schedules
-        List<Schedule> scheduleList = new ArrayList<>();
-        for (Pet pet: pets) {
-            scheduleList.addAll(scheduleRepository.findByScheduledPets(pet));
-        }
-        // remove duplicates and convert to DTO
-        return  scheduleList.stream().distinct().map(this::toDTo).collect(Collectors.toList());
-    }
+  public Schedule createSchedule(Schedule schedule) {
+    return this.save(schedule);
+  }
 
-    @Transactional
-    public ScheduleDTO createSchedule(ScheduleDTO scheduleDTO) {
-        Schedule newSchedule = toEntity(scheduleDTO);
-        return toDTo(this.save(newSchedule));
-    }
-
-    private ScheduleDTO toDTo(Schedule schedule) {
-        ScheduleDTO dto = modelMapper.map(schedule, ScheduleDTO.class);
-        dto.setEmployeeIds(schedule.getScheduledEmployees()
-                .stream()
-                .map(employee -> employee.getId())
-                .collect(Collectors.toList()));
-        dto.setPetIds(schedule.getScheduledPets()
-                .stream()
-                .map(pet -> pet.getId())
-                .collect(Collectors.toList()));
-        return dto;
-    }
-
-    private Schedule toEntity(ScheduleDTO dto) {
-        Schedule schedule = modelMapper.map(dto, Schedule.class);
-        // add other entities
-        List<Employee> employeeList = dto.getEmployeeIds()
-                .stream()
-                .map(empId -> {
-                    return employeeService.get(empId);
-                })
-                .collect(Collectors.toList());
-        schedule.setScheduledEmployees(employeeList);
-        List<Pet> petList = dto.getPetIds()
-                .stream()
-                .map(petId -> {
-                    return petService.get(petId);
-                })
-                .collect(Collectors.toList());
-        schedule.setScheduledPets(petList);
-        // pets
-        return schedule;
-    }
-
-    @Transactional
-    private Schedule save(Schedule schedule) {
-        return scheduleRepository.save(schedule);
-    }
-
-    private Schedule get(Long id) {
-        return scheduleRepository.findById(id).get();
-    }
+  @Transactional
+  private Schedule save(Schedule schedule) {
+    return scheduleRepository.save(schedule);
+  }
 }
